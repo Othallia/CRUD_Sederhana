66984ba27bb5165efa2beb3592323949de3a7a71
Othallia <vianiputriocta6@gmail.com>
2025-03-20 13:49:57 +0700

    menambahkan elemen ke dalam textbox

--- CRUD_Sederhana/Form1.cs
+++ CRUD_Sederhana/Form1.cs
@@ -14,4 +14,24 @@ namespace CRUD_Sederhana
     public partial class Form1 : Form
     {
         private string ConnectionString = "Data Source = OCTAVIANIPTR\\OTHALLIA;" + "Initial Catalog =OrganisasiMahasiswaa;" + "Integrated Security = True";
-     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void ClearFrom()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+            label1.Focus();
+        }
+}
