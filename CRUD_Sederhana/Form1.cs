c369b607a1af31e2faeadd789134bb7490dc7948
Othallia <vianiputriocta6@gmail.com>
2025-03-20 13:51:17 +0700

    menambahkan data mahasiswa dari sql

--- CRUD_Sederhana/Form1.cs
+++ CRUD_Sederhana/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CRUD_Sederhana
 {
@@ -34,4 +35,28 @@ namespace CRUD_Sederhana
 
             label1.Focus();
         }
-}
+    }
+    private void LoadData()
+    {
+        using (SqlConnection conn = new SqlConnection(ConnectionString))
+        {
+            try
+            {
+                conn.Open();
+                string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvMahasiswa.AutoGenerateColumns = true;
+                dgvMahasiswa.DataSource = dt;
+
+                ClearFrom();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    
