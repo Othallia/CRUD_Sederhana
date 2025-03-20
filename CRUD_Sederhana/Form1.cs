c55957768217d98a7675ad3b698211caa2ef5f9a
Othallia <vianiputriocta6@gmail.com>
2025-03-20 14:22:34 +0700

    ngejalanin query

--- CRUD_Sederhana/Form1.cs
+++ CRUD_Sederhana/Form1.cs
@@ -42,7 +42,7 @@ namespace CRUD_Sederhana
                 try
                 {
                     conn.Open(); //connect
-                    string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
+                    string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa"; //data dari sql
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
                     DataTable dt = new DataTable();
                     da.Fill(dt); 
